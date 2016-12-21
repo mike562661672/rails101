26038ccc010a0c304068611a132cedd6d0a22f3d
mike562661672 <mike562661672@gmail.com>
2016-12-21 20:02:35 +0800

    implement my post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,4 +2,13 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def new
+    @post =Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
 end
